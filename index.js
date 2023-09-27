0cdee8f77818e6fbfdab2aff04b68a5f1ed180b1
dloetzke <97966457+dloetzke@users.noreply.github.com>
2023-09-27 09:20:31 +0200

    Readded formatting of JSON output

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const payload = JSON.stringify(github.context.payload);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
   core.setOutput("payload", payload);
 } catch(error) {
